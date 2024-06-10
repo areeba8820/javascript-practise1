347d6ba8f7daeef47f1f046389f739e443afc17b
areeba8820 <162459943+areeba8820@users.noreply.github.com>
2024-06-10 15:49:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// for( let i = 60; i > 5; i = i-6){
+//     document.write(i + "<br>");
+// }
+
+
+// for(let i = 9; i < 91; i = i + 9){
+//     console.log(i);
+// }
+
+// let name = "Areeba Huzaifa"
+
+// for (let i = 0; i < name.length; i++){
+//     console.log(name.length);
+// }
+
+// var num1 = 5
+// var num2 = 3
+
+// var result1 = num1 + num2
+// var result2 = result1 * 2
+// console.log(result2);
+
+
+// var length = 10;
+// var width = 5;
+// var areaRectangle = length * width
+// console.log(areaRectangle);
+
+
+// let x = 5;
+// let y = x++;
+// console.log(x);
+// console.log(y);
+
+let a = 7;
+let b = ++a;
+let c = a--;
+console.log(a);
+console.log(b);
+
+let x = 10;
+let y = ++x;
+console.log(x);
+console.log(y);
+
+
